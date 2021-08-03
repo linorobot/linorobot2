fbd12616392a580355e127d4600de9d5fa634fb0
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-04 04:39:24 +0800

    added ekf

--- linorobot2_bringup/launch/bringup.launch.py
+++ linorobot2_bringup/launch/bringup.launch.py
@@ -1,11 +1,16 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    ekf_config_path = PathJoinSubstitution(
+        [FindPackageShare("linorobot2_base"), "config", "ekf.yaml"]
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             name='serial_port', 
@@ -19,5 +24,15 @@ def generate_launch_description():
             name='micro_ros_agent',
             output='screen',
             arguments=['serial', '--dev', LaunchConfiguration("serial_port")]
+        ),
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[
+                ekf_config_path
+            ],
+            remappings=[("odometry/filtered", "odom")]
         )
     ])

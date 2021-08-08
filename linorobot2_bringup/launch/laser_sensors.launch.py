3349056430408a0d2d5e7676b6174db8923ae77b
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-09 01:10:28 +0800

    added supported sensors

---
+++ linorobot2_bringup/launch/laser_sensors.launch.py
@@ -0,0 +1,36 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import LaunchConfigurationEquals
+
+
+def generate_launch_description():
+    rplidar_launch_path = PathJoinSubstitution(
+        [FindPackageShare('rplidar_ros'), 'launch', 'rplidar.launch.py']
+    )
+
+    ldlidar_launch_path = PathJoinSubstitution(
+        [FindPackageShare('ldlidar'), 'launch', 'ldlidar.launch.py']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='laser_sensor', 
+            default_value='',
+            description='Laser Sensor'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rplidar_launch_path),
+            condition=LaunchConfigurationEquals("laser_sensor", 'rplidar')
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(ldlidar_launch_path),
+            condition=LaunchConfigurationEquals("laser_sensor", 'ldlidar')
+        )
+    ])

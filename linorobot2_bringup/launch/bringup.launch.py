22465d64fb3f62eefd6e01f84888d0aa81cc9b6b
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-09 01:10:46 +0800

    added supported sensors

--- linorobot2_bringup/launch/bringup.launch.py
+++ linorobot2_bringup/launch/bringup.launch.py
@@ -1,19 +1,19 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.conditions import LaunchConfigurationNotEquals
+
 
 def generate_launch_description():
-    # rplidar_launch_path = PathJoinSubstitution(
-    #     [FindPackageShare('rplidar_ros'), 'launch', 'rplidar.launch.py']
-    # )
+    laser_sensor = os.getenv('LINOROBOT2_LASER_SENSOR', '')
 
-    # ldlidar_launch_path = PathJoinSubstitution(
-    #     [FindPackageShare('ldlidar'), 'launch', 'ldlidar.launch.py']
-    # )
+    laser_sensors_launch_path = PathJoinSubstitution(
+        [FindPackageShare('linorobot2_bringup'), 'launch', 'laser_sensors.launch.py']
+    )
 
     description_launch_path = PathJoinSubstitution(
         [FindPackageShare('linorobot2_description'), 'launch', 'description.launch.py']
@@ -50,14 +50,9 @@ def generate_launch_description():
         ),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(description_launch_path)
-        ),
-
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(rplidar_launch_path)
-        # )
-
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(ldlidar_launch_path)
-        # )
+            PythonLaunchDescriptionSource(laser_sensors_launch_path),
+            launch_arguments={
+                'laser_sensor': laser_sensor
+            }.items()        
+        )
     ])

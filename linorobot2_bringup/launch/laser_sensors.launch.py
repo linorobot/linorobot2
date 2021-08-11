8ff2211206c760bd6942ab11e28f6911cdbb88ea
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-11 21:34:06 +0800

    removed unused library

--- linorobot2_bringup/launch/laser_sensors.launch.py
+++ linorobot2_bringup/launch/laser_sensors.launch.py
@@ -1,9 +1,8 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import LaunchConfigurationEquals
 

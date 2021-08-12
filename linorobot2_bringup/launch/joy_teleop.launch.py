20abd14b1a24af4267459129ac01dbe4dd5e9afc
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-12 19:05:08 +0800

    changed joy to joy_linux

--- linorobot2_bringup/launch/joy_teleop.launch.py
+++ linorobot2_bringup/launch/joy_teleop.launch.py
@@ -10,9 +10,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='joy',
-            executable='joy_node',
-            name='joy_node',
+            package='joy_linux',
+            executable='joy_linux_node',
+            name='joy_linux_node',
             output='screen',
         ),
 

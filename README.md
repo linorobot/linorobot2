79a982c7fdc26fb6bba51f7578a718428fc019be
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-09-01 00:46:58 +0800

    added fake laser sensor

--- README.md
+++ README.md
@@ -82,13 +82,16 @@ The launch files of the tested laser sensors have already been added in bringup.
 Tested Laser Sensors:
 - `rplidar` - [RP LIDAR A1](https://www.slamtec.com/en/Lidar/A1)
 - `ldlidar` - [LD06 LIDAR](https://www.inno-maker.com/product/lidar-ld06/)
+- `realsense` - [Intel RealSense](https://www.intelrealsense.com/stereo-depth/) D435, D435i
 
 For example:
 
     echo "export LINOROBOT2_LASER_SENSOR=rplidar" >> ~/.bashrc
 
+If you export realsense to `LINOROBOT2_LASER_SENSOR`, the launch file will run depthimage_to_laserscan[https://github.com/ros-perception/depthimage_to_laserscan] to convert the depth sensor's depth image to laser.
+
 #### 1.2.c Depth Sensor (Optional)
-The Nav2 config file has been configured to support Voxel Layer for marking 3D obstacles in the Global Costmap using a depth sensor. To enable one of the tested depth sensor's launch file in bringup.launch.py, export the depth sensor you're using to `LINOROBOT2_DEPTH_SENSOR` env variable.
+The Nav2 config file has been configured to support Voxel Layer for marking 3D obstacles in the Local Costmap using a depth sensor. To enable one of the tested depth sensor's launch file in bringup.launch.py, export the depth sensor you're using to `LINOROBOT2_DEPTH_SENSOR` env variable.
 
 Tested sensors are:
 - `realsense` - [Intel RealSense](https://www.intelrealsense.com/stereo-depth/) D435, D435i
--- linorobot2_bringup/launch/sensors.launch.py
+++ linorobot2_bringup/launch/sensors.launch.py
@@ -5,11 +5,16 @@ from launch.substitutions import PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     laser_sensor = os.getenv('LINOROBOT2_LASER_SENSOR', '')
     depth_sensor = os.getenv('LINOROBOT2_DEPTH_SENSOR', '')
+
+    fake_laser_config_path = PathJoinSubstitution(
+        [FindPackageShare("linorobot2_bringup"), "config", "fake_laser.yaml"]
+    )
 
     rplidar_launch_path = PathJoinSubstitution(
         [FindPackageShare('rplidar_ros'), 'launch', 'rplidar.launch.py']
@@ -22,7 +27,7 @@ def generate_launch_description():
     realsense_launch_path = PathJoinSubstitution(
         [FindPackageShare('realsense2_camera'), 'launch', 'rs_launch.py']
     )
-    
+
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(rplidar_launch_path),
@@ -41,6 +46,16 @@ def generate_launch_description():
                 'filters': 'pointcloud',
                 'ordered_pc': 'true'
             }.items()   
+        ),
+
+        Node(
+            condition=IfCondition(PythonExpression(['"realsense" == "', laser_sensor, '"'])),
+            package='depthimage_to_laserscan',
+            node_executable='depthimage_to_laserscan_node',
+            node_name='depthimage_to_laserscan_node',
+            remappings=[('depth','/camera/depth/image_rect_raw'),
+                        ('depth_camera_info', '/camera/depth/camera_info')],
+            parameters=[fake_laser_config_path]
         )
     ])
 

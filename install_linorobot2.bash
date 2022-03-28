ee2ffe6689a44745b51a7a5e7fb2b4ba0faf02ea
Juan Jimeno <jimenojmm@gmail.com>
2022-03-28 12:40:16 +0800

    updated ROS_DISTRO based installation

--- install_linorobot2.bash
+++ install_linorobot2.bash
@@ -248,7 +248,7 @@ source $WORKSPACE/install/setup.bash
 
 #### 2.1 Download linorobot2:
 cd $WORKSPACE
-git clone https://github.com/linorobot/linorobot2 src/linorobot2
+git clone -b $ROS_DISTRO https://github.com/linorobot/linorobot2 src/linorobot2
 
 #### 2.2 Ignore Gazebo Packages on robot computer (optional)
 cd $WORKSPACE/src/linorobot2/linorobot2_gazebo

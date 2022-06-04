0aad8a4ce1f0ad503b3cd1649a7ee4e217b72ce4
Juan Jimeno <jimenojmm@gmail.com>
2022-06-05 02:34:26 +0800

    fixed use_sim_time error

--- linorobot2_description/launch/description.launch.py
+++ linorobot2_description/launch/description.launch.py
@@ -61,10 +61,10 @@ def generate_launch_description():
             package='joint_state_publisher',
             executable='joint_state_publisher',
             name='joint_state_publisher',
-            condition=IfCondition(LaunchConfiguration("publish_joints")),
-            parameters=[
-                {'use_sim_time': LaunchConfiguration('use_sim_time')}
-            ]
+            condition=IfCondition(LaunchConfiguration("publish_joints"))
+            # parameters=[
+            #     {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            # ] #since galactic use_sim_time gets passed somewhere and rejects this when defined from launch file
         ),
 
         Node(

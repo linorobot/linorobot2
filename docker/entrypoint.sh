7d2d3b0a8f7d07a9b58124dbc2611947610bd937
Juan Jimeno <jimenojmm@gmail.com>
2023-08-18 07:57:45 -0400

    auto parse and chaneg ROS_DISTRO on entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /opt/ros/humble/setup.bash
+source /opt/ros/distro/setup.bash
 source /root/linorobot2_ws/install/setup.bash
 export FASTRTPS_DEFAULT_PROFILES_FILE=/root/.shm_off.xml
 

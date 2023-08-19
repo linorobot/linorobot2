72492e8b9ea32debbacd8f44e5ab6092e08fde24
Juan Jimeno <jimenojmm@gmail.com>
2023-08-19 11:17:01 +0800

    take rosdistro from env file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /opt/ros/distro/setup.bash
+source /opt/ros/rosdistro/setup.bash
 source /root/linorobot2_ws/install/setup.bash
 export FASTRTPS_DEFAULT_PROFILES_FILE=/root/.shm_off.xml
 

6d242eeb3382b20a0edf502bd1d6b2c9416471d4
Juan Jimeno <jimenojmm@gmail.com>
2023-08-28 23:11:16 +0800

    gazebo humble port

--- linorobot2_gazebo/launch/gazebo.launch.py
+++ linorobot2_gazebo/launch/gazebo.launch.py
@@ -24,6 +24,10 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     use_sim_time = True
 
+    gazebo_launch_path = PathJoinSubstitution(
+        [FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py']
+    )
+
     joy_launch_path = PathJoinSubstitution(
         [FindPackageShare('linorobot2_bringup'), 'launch', 'joy_teleop.launch.py']
     )
@@ -33,7 +37,7 @@ def generate_launch_description():
     )
 
     world_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_gazebo"), "worlds", "playground.world"]
+        [FindPackageShare("linorobot2_gazebo"), "worlds", "playground.sdf"]
     )
 
     description_launch_path = PathJoinSubstitution(
@@ -47,48 +51,49 @@ def generate_launch_description():
             description='Gazebo world'
         ),
 
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', LaunchConfiguration('world')],
-            output='screen'
-        ),
-
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            name='urdf_spawner',
-            output='screen',
-            arguments=["-topic", "robot_description", "-entity", "linorobot2"]
-        ),
-
-        Node(
-            package='linorobot2_gazebo',
-            executable='command_timeout.py',
-            name='command_timeout'
-        ),
-
-        Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            parameters=[
-                {'use_sim_time': use_sim_time}, 
-                ekf_config_path
-            ],
-            remappings=[("odometry/filtered", "odom")]
-        ),
-
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(description_launch_path),
+            PythonLaunchDescriptionSource(gazebo_launch_path),
             launch_arguments={
-                'use_sim_time': str(use_sim_time),
-                'publish_joints': 'false',
+                'gz_args': world_path
             }.items()
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(joy_launch_path),
         )
+
+        # Node(
+        #     package='ros_gz_sim',
+        #     executable='create',
+        #     output='screen'
+        #     arguments=['-topic', 'robot_description', '-name', "linorobot2",],
+        # ),
+
+        # Node(
+        #     package='linorobot2_gazebo',
+        #     executable='command_timeout.py',
+        #     name='command_timeout'
+        # ),
+
+        # Node(
+        #     package='robot_localization',
+        #     executable='ekf_node',
+        #     name='ekf_filter_node',
+        #     output='screen',
+        #     parameters=[
+        #         {'use_sim_time': use_sim_time}, 
+        #         ekf_config_path
+        #     ],
+        #     remappings=[("odometry/filtered", "odom")]
+        # ),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(description_launch_path),
+        #     launch_arguments={
+        #         'use_sim_time': str(use_sim_time),
+        #         'publish_joints': 'false',
+        #     }.items()
+        # ),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(joy_launch_path),
+        # )
     ])
 
 #sources: 

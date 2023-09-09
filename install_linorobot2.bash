330e3aa617a3576cab5288b9add21d560374e8ae
Michael Jonathan <michaeljonathan664@gmail.com>
2023-09-09 08:29:42 +0700

    Update install_linorobot2.bash change the oak d camera naming to match the params
    (cherry picked from commit 8b2f0de1610b47b554971909f924e797fb7964ef)

--- install_linorobot2.bash
+++ install_linorobot2.bash
@@ -146,17 +146,13 @@ function install_zed2i {
     install_zed
 }
 
-function install_oakd{
+function install_OAK-D{
     echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
     sudo udevadm control --reload-rules && sudo udevadm trigger
     sudo apt install ros-$ROS_DISTRO-depthai-ros
 }
 
-function install_oakdlite{
-    install_oakd
-}
-
-function install_oakdpro{
+function install_OAK-D-LITE{
     install_oakd
 }
 

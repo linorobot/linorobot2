508aeb68ea8c21bf0ed3a75cf754e721e6b9c9ce
Juan Miguel Jimeno <jimenojmm@gmail.com>
2024-02-08 12:08:15 +0800

    parameterized spawn position and heading

--- linorobot2_gazebo/launch/gazebo.launch.py
+++ linorobot2_gazebo/launch/gazebo.launch.py
@@ -45,6 +45,30 @@ def generate_launch_description():
             description='Gazebo world'
         ),
 
+        DeclareLaunchArgument(
+            name='spawn_x', 
+            default_value='0.0',
+            description='Robot spawn position in X axis'
+        ),
+
+        DeclareLaunchArgument(
+            name='spawn_y', 
+            default_value='0.0',
+            description='Robot spawn position in Y axis'
+        ),
+
+        DeclareLaunchArgument(
+            name='spawn_z', 
+            default_value='0.0',
+            description='Robot spawn position in Z axis'
+        ),
+            
+        DeclareLaunchArgument(
+            name='spawn_yaw', 
+            default_value='0.0',
+            description='Robot spawn heading'
+        ),
+
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', LaunchConfiguration('world')],
             output='screen'
@@ -55,7 +79,14 @@ def generate_launch_description():
             executable='spawn_entity.py',
             name='urdf_spawner',
             output='screen',
-            arguments=["-topic", "robot_description", "-entity", "linorobot2"]
+            arguments=[
+                '-topic', 'robot_description', 
+                '-entity', 'linorobot2', 
+                '-x', LaunchConfiguration('spawn_x'),
+                '-y', LaunchConfiguration('spawn_y'),
+                '-z', LaunchConfiguration('spawn_z'),
+                '-Y', LaunchConfiguration('spawn_yaw'),
+            ]
         ),
 
         Node(

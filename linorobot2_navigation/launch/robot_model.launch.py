ddaa4993dff1000884d2112b339786b2cbc16b69
Marawan-Ahmed <marawanahrabee@gmail.com>
2024-03-06 02:16:58 +0200

    removed the need for linorobot_viz

---
+++ linorobot2_navigation/launch/robot_model.launch.py
@@ -0,0 +1,21 @@
+import os
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('linorobot2_viz'), 'rviz', 'description.rviz']
+    )
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+        )
+    ])

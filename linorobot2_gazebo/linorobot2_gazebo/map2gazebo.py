b512fb4af5c36784136aa495c6705b2dc6783e81
Juan Miguel Jimeno <jimenojmm@gmail.com>
2025-02-22 19:08:17 +0800

    improved wall color and modularized model

--- linorobot2_gazebo/linorobot2_gazebo/map2gazebo.py
+++ linorobot2_gazebo/linorobot2_gazebo/map2gazebo.py
@@ -7,186 +7,6 @@ import argparse
 import os
 import sys
 
-XML_WORLD_TEMPLATE="""
-<sdf version='1.7'>
-  <world name='default'>
-    <light name='sun' type='directional'>
-      <cast_shadows>1</cast_shadows>
-      <pose>0 0 10 0 -0 0</pose>
-      <diffuse>0.8 0.8 0.8 1</diffuse>
-      <specular>0.2 0.2 0.2 1</specular>
-      <attenuation>
-        <range>1000</range>
-        <constant>0.9</constant>
-        <linear>0.01</linear>
-        <quadratic>0.001</quadratic>
-      </attenuation>
-      <direction>-0.5 0.1 -0.9</direction>
-      <spot>
-        <inner_angle>0</inner_angle>
-        <outer_angle>0</outer_angle>
-        <falloff>0</falloff>
-      </spot>
-    </light>
-    <model name='ground_plane'>
-      <static>1</static>
-      <link name='link'>
-        <collision name='collision'>
-          <geometry>
-            <plane>
-              <normal>0 0 1</normal>
-              <size>100 100</size>
-            </plane>
-          </geometry>
-          <surface>
-            <friction>
-              <ode>
-                <mu>100</mu>
-                <mu2>50</mu2>
-              </ode>
-              <torsional>
-                <ode/>
-              </torsional>
-            </friction>
-            <contact>
-              <ode/>
-            </contact>
-            <bounce/>
-          </surface>
-          <max_contacts>10</max_contacts>
-        </collision>
-        <visual name='visual'>
-          <cast_shadows>0</cast_shadows>
-          <geometry>
-            <plane>
-              <normal>0 0 1</normal>
-              <size>100 100</size>
-            </plane>
-          </geometry>
-          <material>
-            <script>
-              <uri>file://media/materials/scripts/gazebo.material</uri>
-              <name>Gazebo/Grey</name>
-            </script>
-          </material>
-        </visual>
-        <self_collide>0</self_collide>
-        <enable_wind>0</enable_wind>
-        <kinematic>0</kinematic>
-      </link>
-    </model>
-    <gravity>0 0 -9.8</gravity>
-    <magnetic_field>6e-06 2.3e-05 -4.2e-05</magnetic_field>
-    <atmosphere type='adiabatic'/>
-    <physics type='ode'>
-      <max_step_size>0.001</max_step_size>
-      <real_time_factor>1</real_time_factor>
-      <real_time_update_rate>1000</real_time_update_rate>
-    </physics>
-    <scene>
-      <ambient>0.4 0.4 0.4 1</ambient>
-      <background>0.7 0.7 0.7 1</background>
-      <shadows>1</shadows>
-    </scene>
-    <audio>
-      <device>default</device>
-    </audio>
-    <wind/>
-    <spherical_coordinates>
-      <surface_model>EARTH_WGS84</surface_model>
-      <latitude_deg>0</latitude_deg>
-      <longitude_deg>0</longitude_deg>
-      <elevation>0</elevation>
-      <heading_deg>0</heading_deg>
-    </spherical_coordinates>
-    <model name='{name}'>
-      <link name='link'>
-        <inertial>
-          <mass>15</mass>
-          <inertia>
-            <ixx>0</ixx>
-            <ixy>0</ixy>
-            <ixz>0</ixz>
-            <iyy>0</iyy>
-            <iyz>0</iyz>
-            <izz>0</izz>
-          </inertia>
-        </inertial>
-        <collision name='collision'>
-          <pose>0 0 0 0 0 0</pose>
-          <geometry>
-            <mesh>
-              <uri>model://{name}/meshes/{name}.stl</uri>
-            </mesh>
-          </geometry>
-          <max_contacts>10</max_contacts>
-          <surface>
-            <contact>
-              <ode/>
-            </contact>
-            <bounce/>
-            <friction>
-              <torsional>
-                <ode/>
-              </torsional>
-              <ode/>
-            </friction>
-          </surface>
-        </collision>
-        <visual name='visual'>
-          <pose>0 0 0 0 0 0</pose>
-          <geometry>
-            <mesh>
-              <uri>model://{name}/meshes/{name}.stl</uri>
-            </mesh>
-          </geometry>
-        </visual>
-        <self_collide>0</self_collide>
-        <enable_wind>0</enable_wind>
-        <kinematic>0</kinematic>
-      </link>
-      <static>1</static>
-      <pose>0 0 0 0 0 0</pose>
-    </model>
-    <state world_name='default'>
-      <sim_time>0 0</sim_time>
-      <real_time>0 0</real_time>
-      <wall_time>0 0</wall_time>
-      <iterations>0</iterations>
-      <model name='{name}'>
-        <pose>0 0 0 0 0 0</pose>
-        <scale>1 1 1</scale>
-        <link name='link'>
-          <pose>0 0 0 0 0 0</pose>
-          <velocity>0 0 0 0 0 0</velocity>
-          <acceleration>0 0 0 0 0 0</acceleration>
-          <wrench>0 0 0 0 0 0</wrench>
-        </link>
-      </model>
-      <model name='ground_plane'>
-        <pose>0 0 0 0 0 0</pose>
-        <scale>1 1 1</scale>
-        <link name='link'>
-          <pose>0 0 0 0 0 0</pose>
-          <velocity>0 0 0 0 0 0</velocity>
-          <acceleration>0 0 0 0 0 0</acceleration>
-          <wrench>0 0 0 0 0 0</wrench>
-        </link>
-      </model>
-      <light name='sun'>
-        <pose>0 0 10 0 0 0</pose>
-      </light>
-    </state>
-    <gui fullscreen='0'>
-      <camera name='user_camera'>
-        <pose>24.5661 -14.3584 14.4948 0 0.503643 2.46819</pose>
-        <view_controller>orbit</view_controller>
-        <projection_type>perspective</projection_type>
-      </camera>
-    </gui>
-  </world>
-</sdf>
-"""
 
 XML_MODEL_CONFIG_TEMPLATE = """
 <?xml version="1.0" ?>
@@ -202,44 +22,125 @@ XML_MODEL_CONFIG_TEMPLATE = """
 </model>
 """
 
+XML_MODEL_TEMPLATE = """
+<model name="{name}">
+  <link name="link">
+    <inertial>
+      <mass>15</mass>
+      <inertia>
+        <ixx>0.0</ixx>
+        <ixy>0.0</ixy>
+        <ixz>0.0</ixz>
+        <iyy>0.0</iyy>
+        <iyz>0.0</iyz>
+        <izz>0.0</izz>
+      </inertia>
+    </inertial>
+    <collision name="collision">
+      <pose>0 0 0 0 0 0</pose>
+      <geometry>
+        <mesh>
+          <uri>model://{name}/meshes/{name}.stl</uri>
+        </mesh>
+      </geometry>
+    </collision>
+    <visual name="visual">
+      <pose>0 0 0 0 0 0</pose>
+      <geometry>
+        <mesh>
+          <uri>model://{name}/meshes/{name}.stl</uri>
+        </mesh>
+      </geometry>
+      <material>
+        <ambient>1 1 1 1</ambient>
+        <diffuse>1 1 1 1</diffuse>
+        <specular>0.5 0.5 0.5 1</specular>
+        <emissive>0 0 0 1</emissive>
+      </material>
+    </visual>
+  </link>
+  <static>1</static>
+</model>
+"""
+
 XML_SDF_TEMPLATE = """
 <?xml version="1.0" ?>
 <sdf version="1.4">
-  <model name="{name}">
-    <link name="link">
-      <inertial>
-        <mass>15</mass>
-        <inertia>
-          <ixx>0.0</ixx>
-          <ixy>0.0</ixy>
-          <ixz>0.0</ixz>
-          <iyy>0.0</iyy>
-          <iyz>0.0</iyz>
-          <izz>0.0</izz>
-        </inertia>
-      </inertial>
-      <collision name="collision">
-        <pose>0 0 0 0 0 0</pose>
-        <geometry>
-          <mesh>
-            <uri>model://{name}/meshes/{name}.stl</uri>
-          </mesh>
-        </geometry>
-      </collision>
-      <visual name="visual">
-        <pose>0 0 0 0 0 0</pose>
-        <geometry>
-          <mesh>
-            <uri>model://{name}/meshes/{name}.stl</uri>
-          </mesh>
-        </geometry>
-      </visual>
-    </link>
-    <static>1</static>
-  </model>
+  {model_template}
 </sdf>
 """
 
+
+XML_WORLD_TEMPLATE="""
+<sdf version="1.8">
+  <world name="playground">
+
+    <gravity>0 0 -9.8</gravity>
+    <physics default="0" name="default_physics" type="ode">
+      <max_step_size>0.01</max_step_size>
+      <real_time_factor>1</real_time_factor>
+      <real_time_update_rate>100</real_time_update_rate>
+    </physics>
+
+    <plugin
+      filename="gz-sim-physics-system"
+      name="gz::sim::systems::Physics">
+    </plugin>
+    <plugin
+      filename="gz-sim-user-commands-system"
+      name="gz::sim::systems::UserCommands">
+    </plugin>
+    <plugin
+      filename="gz-sim-scene-broadcaster-system"
+      name="gz::sim::systems::SceneBroadcaster">
+    </plugin>
+
+    <light name="sun" type="directional">
+      <cast_shadows>true</cast_shadows>
+      <pose>0 0 10 0 0 0</pose>
+      <diffuse>0.8 0.8 0.8 1</diffuse>
+      <specular>0.2 0.2 0.2 1</specular>
+      <attenuation>
+        <range>1000</range>
+        <constant>0.9</constant>
+        <linear>0.01</linear>
+        <quadratic>0.001</quadratic>
+      </attenuation>
+      <direction>-0.5 0.1 -0.9</direction>
+    </light>
+
+    <model name="ground_plane">
+      <static>true</static>
+      <link name="link">
+        <collision name="collision">
+          <geometry>
+            <plane>
+              <normal>0 0 1</normal>
+              <size>100 100</size>
+            </plane>
+          </geometry>
+        </collision>
+        <visual name="visual">
+          <geometry>
+            <plane>
+              <normal>0 0 1</normal>
+              <size>100 100</size>
+            </plane>
+          </geometry>
+          <material>
+            <ambient>0.8 0.8 0.8 1</ambient>
+            <diffuse>0.8 0.8 0.8 1</diffuse>
+            <specular>0.8 0.8 0.8 1</specular>
+          </material>
+        </visual>
+      </link>
+    </model>
+
+    {model_template}
+    
+  </world>
+</sdf>
+"""
 
 class MapConverter:
     def __init__(self, map_dir, export_dir, world_dir, height=1.5):
@@ -287,7 +188,8 @@ class MapConverter:
             sdf_dir = self.export_dir + f'{key}/' + f'{key}.sdf'
             config_dir = self.export_dir + f'{key}/model.config'
 
-            sdf_data = XML_SDF_TEMPLATE.format(name=key)
+            model_template = XML_MODEL_TEMPLATE.format(name=key)
+            sdf_data = XML_SDF_TEMPLATE.format(model_template=model_template)
             config_data = XML_MODEL_CONFIG_TEMPLATE.format(name=key)
             print(f'export file: {stl_dir}')
             
@@ -301,9 +203,9 @@ class MapConverter:
                 f.write(config_data)
 
             # create world file
-            world_data = XML_WORLD_TEMPLATE.format(name=key)
+            world_data = XML_WORLD_TEMPLATE.format(model_template=model_template)
 
-            world_dir = self.world_dir + f'{key}.world'
+            world_dir = self.world_dir + f'{key}.sdf'
             with open(world_dir, 'w') as f:
                 f.write(world_data)
 
@@ -362,7 +264,7 @@ class MapConverter:
         if not mesh.is_volume:
             mesh.fix_normals()
         
-        mesh.remove_duplicate_faces()
+        mesh.update_faces(mesh.unique_faces())
         
         return mesh
 

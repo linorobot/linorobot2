da1d95c15106ee538ad0f9cfb898e48e399f941b
Juan Jimeno <jimenojmm@gmail.com>
2025-11-01 18:01:05 +0800

    fixed user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/home/$USER/linorobot2_ws/install/setup.bash"
+source "/home/ros/linorobot2_ws/install/setup.bash"
 exec "$@"

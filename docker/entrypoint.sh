b87dd0ebc63df744e5c22fc54e1f7a7a35b862dd
Juan Jimeno <jimenojmm@gmail.com>
2026-02-19 08:33:04 +0000

    made workspace dynamic

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/home/ros/linorobot2_ws/install/setup.bash"
+source "$WORKSPACE/install/setup.bash"
 
 
 if [ "${VIRTUALGL_ENABLED}" = "true" ] && [ -x "$(command -v vglrun)" ]; then

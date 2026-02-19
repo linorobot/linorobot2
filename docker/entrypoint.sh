bbe69014aebc7be8d627aa6b45dd6fa8ae29f1a2
Juan Jimeno <jimenojmm@gmail.com>
2026-02-19 01:36:36 +0000

    removed unused files and simplified Dockerfile without setting RMW, and moved virtualgl to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,10 @@
 set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/home/ros/linorobot2_ws/install/setup.bash"
-exec "$@"
+
+
+if [ "${VIRTUALGL_ENABLED}" = "true" ] && [ -x "$(command -v vglrun)" ]; then
+    exec vglrun +v -d /dev/dri/card${GPU_ID:-0} "$@"
+else
+    exec "$@"
+fi
